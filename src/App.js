b87f6632cdfde1bf8901ce50366625098760aea3
omar258 <omar.mmohhammedd@gmail.com>
2022-06-23 19:25:07 +0200

    none

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
           <Route path='/menu' element={<Menu />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
